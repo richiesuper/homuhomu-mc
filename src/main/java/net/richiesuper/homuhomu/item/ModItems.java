6cea23fbffd2026acdf9c0346ffc8ce5cbdb87fb
Richie Seputro <rcesptro@ctemplar.com>
2022-09-03 20:39:37 +0700

    Added soul gem and grief seed items.

---
+++ src/main/java/net/richiesuper/homuhomu/item/ModItems.java
@@ -0,0 +1,21 @@
+package net.richiesuper.homuhomu.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import net.richiesuper.homuhomu.HomuHomu;
+
+public class ModItems {
+    public static final Item GRIEF_SEED = registerItem("grief_seed", new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+    public static final Item SOUL_GEM = registerItem("soul_gem", new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(HomuHomu.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        HomuHomu.LOGGER.info("Registering Mod Items for " + HomuHomu.MOD_ID);
+    }
+}
