487d613dbe473a780c605bc970e2384fb57bd096
Richie Seputro <rcesptro@ctemplar.com>
2022-09-17 01:24:51 +0700

    add HomuHomuClient.java

---
+++ src/main/java/net/richiesuper/homuhomu/HomuHomuClient.java
@@ -0,0 +1,10 @@
+package net.richiesuper.homuhomu;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HomuHomuClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
