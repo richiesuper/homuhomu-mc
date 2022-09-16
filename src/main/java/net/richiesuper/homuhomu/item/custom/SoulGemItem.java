45fd633bc104b8761f0da3396a8f226184fb338c
Richie Seputro <rcesptro@ctemplar.com>
2022-09-17 01:36:22 +0700

    Update SoulGemItem.java
    Some changes in Soul Gem item behavior. It no longer grants players the Magical effect (it now can only be obtained through kyuubee). The soul gem also glints now. The soul gem now will kill the player and break itself if it's used to mine blocks / attack entities. Effects duration will be proportional to the use ticks taken (i.e. how long you hold the right mouse button before releasing it).

--- src/main/java/net/richiesuper/homuhomu/item/custom/SoulGemItem.java
+++ src/main/java/net/richiesuper/homuhomu/item/custom/SoulGemItem.java
@@ -1,46 +1,78 @@
 package net.richiesuper.homuhomu.item.custom;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
-import net.minecraft.util.Hand;
-import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.richiesuper.homuhomu.effect.ModEffects;
 
 public class SoulGemItem extends Item {
+    private static final int BASE_COOLDOWN = 4800;
+    private static final int BASE_EFFECT_DURATION = 2400;
+
+    private static int usageTickCount = 0;
+
     public SoulGemItem(Settings settings) {
         super(settings);
     }
 
-	// TODO:
-	// Make it so that when player uses soul gem / grief seed to attack mobs / destroy blocks,
-	// the soul gem / grief seed immediately break. Keyword here is BREAK.
-	// Soul Gem breakage just kills the player and does NOT spawn a grief seed.
-	// Grief Seed breakage spawns a witch in the vicinity.
-
     @Override
-    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-        if (!world.isClient()) {
-			user.addStatusEffect(new StatusEffectInstance(ModEffects.MAGICAL, 2400, 0, true, true, true));
-			user.addStatusEffect(new StatusEffectInstance(StatusEffects.ABSORPTION, 2400, 2, true, true, true));
-			user.addStatusEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 2400, 2, true, true, true));
-			user.addStatusEffect(new StatusEffectInstance(StatusEffects.HASTE, 2400, 1, true, true, true));
-			user.addStatusEffect(new StatusEffectInstance(StatusEffects.HEALTH_BOOST, 2400, 2, true, true, true));
-			user.addStatusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 2400, 0, true, true, true));
-			user.addStatusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 2400, 0, true, true, true));
-
-			declareMagical(user);
-			user.getItemCooldownManager().set(this, 4800);
-        }
-
-		return super.use(world, user, hand);
+    public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+        stack.damage(stack.getMaxDamage() - stack.getDamage(), attacker, e -> e.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND));
+        attacker.kill();
+        return true;
     }
 
-	private void declareMagical(PlayerEntity player) {
-		player.sendMessage(Text.literal(player.getEntityName() + " is now a magical entity! Witches begone!"));
-	}
+    @Override
+    public boolean postMine(ItemStack stack, World world, BlockState state, BlockPos pos, LivingEntity miner) {
+        stack.damage(stack.getMaxDamage() - stack.getDamage(), miner, e -> e.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND));
+        miner.kill();
+        return true;
+    }
+
+    @Override
+    public void usageTick(World world, LivingEntity user, ItemStack stack, int remainingUseTicks) {
+        usageTickCount++;
+    }
+
+    @Override
+    public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
+        if (!world.isClient()) {
+            PlayerEntity player = (PlayerEntity) user;
+
+            int multiplier = usageTickCount / 20;
+            int duration = BASE_EFFECT_DURATION * multiplier;
+            int cooldown = BASE_COOLDOWN * multiplier;
+
+            user.addStatusEffect(new StatusEffectInstance(ModEffects.TRANSFORMED, duration, 0, true, true, true));
+            user.addStatusEffect(new StatusEffectInstance(StatusEffects.ABSORPTION, duration, 2, true, true, true));
+            user.addStatusEffect(new StatusEffectInstance(StatusEffects.STRENGTH, duration, 2, true, true, true));
+            user.addStatusEffect(new StatusEffectInstance(StatusEffects.HASTE, duration, 1, true, true, true));
+            user.addStatusEffect(new StatusEffectInstance(StatusEffects.HEALTH_BOOST, duration, 2, true, true, true));
+            user.addStatusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, duration, 0, true, true, true));
+            user.addStatusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, duration, 0, true, true, true));
+
+            declareMagical(player);
+            player.getItemCooldownManager().set(this, cooldown);
+        }
+
+        usageTickCount = 0;
+        return stack;
+    }
+
+    @Override
+    public boolean hasGlint(ItemStack stack) {
+        return true;
+    }
+
+    private void declareMagical(PlayerEntity player) {
+        player.sendMessage(Text.literal(player.getEntityName() + " is now a magical entity! Witches begone!"));
+    }
 }
