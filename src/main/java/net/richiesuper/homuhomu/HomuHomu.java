38995c8df35d44c1fc9c6d99fbcda1a2fe06d1e7
Richie Seputro <rcesptro@ctemplar.com>
2022-09-17 01:30:19 +0700

    fix whitespace in HomuHomu.java and ModMixin.java

--- src/main/java/net/richiesuper/homuhomu/HomuHomu.java
+++ src/main/java/net/richiesuper/homuhomu/HomuHomu.java
@@ -8,13 +8,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class HomuHomu implements ModInitializer {
-	public static final String MOD_ID = "homuhomu";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final String MOD_ID = "homuhomu";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	@Override
-	public void onInitialize() {
-		ModItems.registerModItems();
-		ModBlocks.registerModBlocks();
-		ModEffects.registerEffects();
-	}
+    @Override
+    public void onInitialize() {
+        ModItems.registerModItems();
+        ModBlocks.registerModBlocks();
+        ModEffects.registerEffects();
+    }
 }
--- src/main/java/net/richiesuper/homuhomu/mixin/ModMixin.java
+++ src/main/java/net/richiesuper/homuhomu/mixin/ModMixin.java
@@ -2,7 +2,6 @@ package net.richiesuper.homuhomu.mixin;
 
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.richiesuper.homuhomu.HomuHomu;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,8 +9,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
 public class ModMixin {
-	@Inject(at = @At("HEAD"), method = "init()V")
-	private void init(CallbackInfo info) {
-		HomuHomu.LOGGER.info("I hope you enjoy the mod!");
-	}
+    @Inject(at = @At("HEAD"), method = "init()V")
+    private void init(CallbackInfo info) {
+        HomuHomu.LOGGER.info("I hope you enjoy the mod!");
+    }
 }
