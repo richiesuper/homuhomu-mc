4516f5b8b922927e1bb72a39070ed119094f659f
Richie Seputro <rcesptro@ctemplar.com>
2022-09-17 21:09:26 +0700

    update SoulGemItem.java
    code clean up and tooltip implementation.

--- src/main/java/net/richiesuper/homuhomu/item/custom/SoulGemItem.java
+++ src/main/java/net/richiesuper/homuhomu/item/custom/SoulGemItem.java
@@ -1,6 +1,7 @@
 package net.richiesuper.homuhomu.item.custom;
 
 import net.minecraft.block.BlockState;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
@@ -9,32 +10,41 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.UseAction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.richiesuper.homuhomu.effect.ModEffects;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class SoulGemItem extends Item {
-    private static final int BASE_COOLDOWN = 600;
-    private static final int BASE_EFFECT_DURATION = 300;
+    private static final int BASE_COOLDOWN = 600; // 30 seconds
+    private static final int BASE_EFFECT_DURATION = 300; // 15 seconds
 
     public SoulGemItem(Settings settings) {
         super(settings);
     }
 
     @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        tooltip.add(Text.literal("Soul Gem is the heart and power source of magical entities!").formatted(Formatting.GOLD));
+    }
+
+    @Override
     public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
         stack.damage(stack.getMaxDamage() - stack.getDamage(), attacker, e -> e.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND));
-        attacker.kill();
+        attacker.sendMessage(Text.literal(attacker.getEntityName() + " destroyed their Soul Gem by using it to attack!"));
         return true;
     }
 
     @Override
     public boolean postMine(ItemStack stack, World world, BlockState state, BlockPos pos, LivingEntity miner) {
         stack.damage(stack.getMaxDamage() - stack.getDamage(), miner, e -> e.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND));
-        miner.kill();
+        miner.sendMessage(Text.literal(miner.getEntityName() + " destroyed their Soul Gem by using it to mine!"));
         return true;
     }
 
@@ -106,6 +116,6 @@ public class SoulGemItem extends Item {
     }
 
     private void declareMagical(PlayerEntity player) {
-        player.sendMessage(Text.literal(player.getEntityName() + " is now a magical entity! Witches begone!"));
+        player.sendMessage(Text.literal(player.getEntityName() + " has now transformed!"));
     }
 }
